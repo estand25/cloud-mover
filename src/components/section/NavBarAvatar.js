70d6cee9d86fb3175fce230a913ffdfa290ed194
Standley Eugene <estand25@gmail.com>
2021-09-09 11:35:38 -0500

    Menu route for login, logout, account, and profile

--- src/components/section/NavBarAvatar.js
+++ src/components/section/NavBarAvatar.js
@@ -2,6 +2,8 @@ import React, { useState} from 'react'
 import { useSigninCheck } from 'reactfire';
 
 import IconButton from '@material-ui/core/IconButton';
+import MuiMenu from './MuiMenu'
+import MuiMenuItem from './MuiMenuItem'
 import { AccountIcon } from '../account';
 
 import { useHistory } from "react-router-dom"
@@ -9,40 +11,60 @@ import { useHistory } from "react-router-dom"
 const NavBarAvatar = () => {
     const history = useHistory()  
     const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
   
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
     const {data: signInCheckResult} = useSigninCheck();
-    const logIn = () => history.push('/logIn')
-    const profile = () => history.push('/profile')
-    const logOut = () => history.push('/logOut')
-    const MyAccount = () => history.push('/myAccount')
+
+    const onMenuHandler = (rte) => {
+      history.push(rte)
+      handleClose()
+    }
+
+    const MenuOptionGeneration = ({list}) => {
+      return list.map(item => {
+        return <MuiMenuItem key={item.label} onClick={item.onClickHandle}>{item.label}</MuiMenuItem>
+      });
+    }
+
+    const layout = !signInCheckResult || !signInCheckResult.signedIn ? 
+    {
+      method: handleClick,
+      menuOptions: [
+        {onClickHandle: () => onMenuHandler('/logIn'), label: 'Log-In'}
+      ]
+    }
+    :
+    {
+      method: handleClick,
+      menuOptions: [
+        {onClickHandle: () => onMenuHandler('/profile'), label: 'Profile' },
+        {onClickHandle: () => onMenuHandler('/myAccount'), label: 'My Account'},
+        {onClickHandle: () => onMenuHandler('/logOut'), label: 'Log-Out'},
+      ]
+    }
 
     return (
       <div>
         <IconButton
           edge="end"
           color="inherit"
-          onClick={typeof(signInCheckResult?.user) !== 'undefined' ? profile: logIn}
+          onClick={layout.method}
         >
           <AccountIcon/>
         </IconButton>      
-        <Menu
-          id="simple-menu"
+        <MuiMenu
+          id="mini-menu"
           anchorEl={anchorEl}
           keepMounted
           open={Boolean(anchorEl)}
           onClose={handleClose}
         >
-          {/* {typeof(signInCheckResult?.user) !== 'undefined' ? */}
-          <MenuItem onClick={profile}>Profile</MenuItem>
-          <MenuItem onClick={MyAccount}>My account</MenuItem>
-          <MenuItem onClick={logOut}>Logout</MenuItem>
-          {/* // :
-          // } */}
-        </Menu>
+          <MenuOptionGeneration
+            list={layout.menuOptions}
+          />
+        </MuiMenu>
       </div>
     )
 }
--- src/components/section/NavBarLogo.js
+++ src/components/section/NavBarLogo.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton';
 import CloudIcon from '@material-ui/icons/Cloud';
-
 import { useHistory } from "react-router-dom"
 
 const NavBarLogo = () => {
