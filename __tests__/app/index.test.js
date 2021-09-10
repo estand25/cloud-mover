88dfc571707b2c5096c380d9dad70e131706cd24
Standley Eugene <estand25@gmail.com>
2021-09-09 22:10:48 -0500

    Mock on hook

--- __tests__/app/index.test.js
+++ __tests__/app/index.test.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from '../../src/app/index.js'
+import { useSigninCheck } from 'reactfire'
+jest.mock('reactfire')
+
+describe("hook 1", () => {
+    it("useSigninCheck", () => {
+        useSigninCheck.mockReturnValue({
+            status: false,
+            data: {
+                
+            }
+        })
+    })
+})
 
 it('render without crashing', () => {
     const app = render(<App />)
