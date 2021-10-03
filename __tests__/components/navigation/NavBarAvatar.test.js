7d5d8e31372645325709f5fb629a7ce53b5f52e2
Standley Eugene <estand25@gmail.com>
2021-10-03 10:28:53 -0500

    Add more unit test to NavBarAvatar

--- __tests__/components/navigation/NavBarAvatar.test.js
+++ __tests__/components/navigation/NavBarAvatar.test.js
@@ -1,20 +1,56 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { NavBarAvatar } from '../../../src/components/navigation'
-import * as reactFire from 'reactfire'
+import * as reactFire from 'reactfire'    
 
 jest.mock("reactfire", () => {
     return {
-        useSigninCheck:  jest.fn().mockResolvedValue({
-            data: {
-                uid: "fakeUid",
-             },
-           }),
+        useSigninCheck:  jest.fn(),
     }
 })
 
-describe('Testing with positive param', () => {    
+
+describe('Testing with  without use data', () => {          
+    beforeEach(() => {
+        useSigninCheckMock.mockImplementation(() => () => {
+            data: null
+         });
+    })
+
+    afterEach(() => {
+        useSigninCheckMock.mockClear();
+    }) 
+
+    
+    const useSigninCheckMock = reactFire.useSigninCheck;
+
     it('render without crashing', () => {
+    
+        const navBarAvatar = render(
+            <NavBarAvatar />
+        )
+
+        expect(navBarAvatar).toBeTruthy();
+    })
+})
+
+describe('Testing with  with use data', () => {          
+    beforeEach(() => {
+        useSigninCheckMock.mockImplementation(() => () => {
+            data: {
+                uid: 'fakeID'
+            }
+         });
+    })
+
+    afterEach(() => {
+        useSigninCheckMock.mockClear();
+    }) 
+
+    const useSigninCheckMock = reactFire.useSigninCheck;
+
+    it('render without crashing', () => {
+    
         const navBarAvatar = render(
             <NavBarAvatar />
         )
